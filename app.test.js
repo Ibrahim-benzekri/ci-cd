97f41eebe1e23e90a253156ce1703496f790b6a6
Ibrahim BENZEKRI ARBAI <123778755+Ibrahim-benzekri@users.noreply.github.com>
2025-04-03 16:07:29 +0200

    test_actions

--- app.test.js
+++ app.test.js
@@ -3,6 +3,6 @@ const { add } = require('./feature/githubpages/app');
 describe('add', () => {
   test('should return the sum of two numbers', () => {
     expect(add(2, 2)).toBe(4);
-    expect(add(-1, 5)).toBe(4);
+    expect(add(-2, 6)).toBe(4);
   });
 });
