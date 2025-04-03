0cc035a03178fd8c93dc2b9dc704963d6099c148
Ibrahim BENZEKRI ARBAI <123778755+Ibrahim-benzekri@users.noreply.github.com>
2025-04-03 15:49:00 +0200

    change 1

--- app.test.js
+++ app.test.js
@@ -2,7 +2,7 @@ const { add } = require('./app');
 
 describe('add', () => {
   test('should return the sum of two numbers', () => {
-    expect(add(1, 2)).toBe(3);
+    expect(add(2, 2)).toBe(4);
     expect(add(-1, 5)).toBe(4);
   });
 });
